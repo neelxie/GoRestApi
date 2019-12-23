e8d9b72afbb0aa2f180ed89b4d6e86519785d36b
Derrick Sekidde <kidricederek@gmail.com>
2019-12-23 22:05:36 +0300

    run api complete.

--- main.go
+++ main.go
@@ -9,3 +9,12 @@ import {
 func homePage(w htttp.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Neelxie my first endpoint")
 }
+
+func handleRequest(){
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main(){
+	handleRequests()
+}
